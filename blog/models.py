773d2cd9d4bcbdbd06f18760f0cc09138f2b35ed
artemwoka <artemwoka@gmail.com>
2026-09-23 19:47:40 +0200

    Register Post model with custom admin interface and add published manager for filtering

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -15,6 +20,10 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length = 2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager() 
+    published = PublishedManager()
+
 
     class Meta:
         ordering = ['-publish']
